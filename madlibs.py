c070b8d5ef5e61783ac99914a32b64f136f2b5c0
Hackbright Student <info@hackbrightacademy.com>
2016-07-19 17:51:06 -0700

    Created mad libs game with amazing background and a good story.

--- madlibs.py
+++ madlibs.py
@@ -38,6 +38,40 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+    """Ask for words!"""
+
+    user_response = request.args.get("play-game")
+
+    if user_response == "yes":
+        return render_template("game.html",
+                           play_game=user_response)
+    else:
+        return render_template("goodbye.html",
+                           play_game=user_response)
+
+
+@app.route('/madlib')
+def show_madlib():
+    """Prints out the madlib."""
+
+    user_person = request.args.get("famous-person")
+    user_color = request.args.get("color")
+    user_noun = request.args.get("noun")
+    user_adjective = request.args.get("adjective")
+    user_place = request.args.get("place")
+    user_heroine = request.args.get("heroine")
+    user_food = request.args.get("food")
+
+    return render_template("madlib.html",
+                            famous=user_person,
+                            color=user_color,
+                            noun=user_noun,
+                            adjective=user_adjective,
+                            place=user_place,
+                            food=user_food,
+                            heroine=user_heroine)
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
